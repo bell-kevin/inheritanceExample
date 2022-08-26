7a6e23b8991731f8f5268de5e6d2ff4283b27479
Kevin Bell <bellKevin@pm.me>
2022-08-25 18:32:54 -0600

    Update fullTimeDriver.java

--- inheritanceExample/src/inheritancexample/fullTimeDriver.java
+++ inheritanceExample/src/inheritancexample/fullTimeDriver.java
@@ -9,7 +9,7 @@ package inheritancexample;
 public class fullTimeDriver {
 
     public static void main(String[] args) {
-        FullTime fullTimer = new FullTime("Kevin", 5733, 100000);
+        FullTime fullTimer = new FullTime("Kevin Bell", 5733, 100000);
         fullTimer.display();
         System.out.println(fullTimer.getName());
     } // end main method
